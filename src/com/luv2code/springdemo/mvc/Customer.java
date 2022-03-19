3531f2f353f67bd09033b58e99ee2377dff93035
Anubhav Choudhary <anubhavchoudhary434@gmail.com>
2022-03-19 10:28:00 +0530

    Course related changes

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
 // for hibernate validations JSR 303 implementation
 public class Customer {
 
@@ -22,6 +24,9 @@ public class Customer {
 	
 	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message="must be 5 chars/digits")
 	private String postalCode;
+	
+	@CourseCode (value = "TOPS", message = "must start with TOPS")   // custom annotation
+	private String courseCode;
 
 	public String getFirstName() {
 		return firstName;
@@ -53,6 +58,14 @@ public class Customer {
 
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
 	}
 	
 	
